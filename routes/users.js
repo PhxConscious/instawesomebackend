7c38fc6c1f42d3a5f939571a7738e51ad6b49631
Ismael Perez <ismaelperezemail@gmail.com>
2018-02-13 13:02:17 -0700

    user data now being posted to database

--- routes/users.js
+++ routes/users.js
@@ -3,25 +3,25 @@ var router = express.Router();
 var knex = require('../db/knex');
 
 /* GET users listing. */
-router.get('/', function (req, res) {
-    knex.raw('select * from user_table')
-        .then(function (user) {
-            res.send(user.rows);
-        })
+router.get('/', function(req, res) {
+  knex.raw('select * from user_table')
+    .then(function(user) {
+      res.send(user.rows);
+    })
 });
 
 //Post new user to database
-router.post('/new', function (req, res, next) {
-    knex.raw(`insert into user_table (email, password, firstName, lastName, userPhone ) values (
-    '${req.body.email}', 
-    '${req.body.password}', 
-    '${req.body.firstName}',
-    '${req.body.lastName}',
-    '${req.body.userPhone}'
+router.post('/new', function(req, res, next) {
+  knex.raw(`insert into user_table (first_name, last_name, user_email, user_phone, firebase_id ) values (
+    '${req.body.first_name}',
+    '${req.body.last_name}',
+    '${req.body.user_email}',
+    '${req.body.user_phone}',
+    '${req.body.firebase_id}'
     )`)
-        .then(() => {
-        res.send('success')
-})
+    .then(() => {
+      res.send('success')
+    })
 });
 
 module.exports = router;
