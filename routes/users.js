c26df60cece249069f374564179006c03dd8ba75
Ismael Perez <ismaelperezemail@gmail.com>
2018-02-13 11:05:59 -0700

    routing for user sign up started

--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,24 @@ var knex = require('../db/knex');
 
 /* GET users listing. */
 router.get('/', function (req, res) {
-    knex.raw('select * from users')
+    knex.raw('select * from user_table')
         .then(function (user) {
             res.send(user.rows);
         })
+});
+
+//Post new user to database
+router.post('/new', function (req, res, next) {
+    knex.raw(`insert into user_table (email, password, firstName, lastName, userPhone ) values (
+    '${req.body.email}', 
+    '${req.body.password}', 
+    '${req.body.firstName}',
+    '${req.body.lastName}',
+    '${req.body.userPhone}'
+    )`)
+        .then(() => {
+        res.send('success')
+})
 });
 
 module.exports = router;
